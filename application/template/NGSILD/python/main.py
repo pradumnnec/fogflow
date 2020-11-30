0eaf18978b0eb5708cf09231f225ff1ed060f1df
root <neeraj.srivastava@india.nec.com>
2020-11-30 17:09:28 +0000

    change python template according to new design

--- application/template/NGSILD/python/main.py
+++ application/template/NGSILD/python/main.py
@@ -5,17 +5,18 @@ import time
 import datetime
 import threading
 import os
+import sys
 
-app = Flask(__name__, static_url_path='')
+import function as fogflow
+
+app = Flask(__name__, static_url_path="")
 
 # global variables
-
 brokerURL = ''
 outputs = []
-timer = None
-lock = threading.Lock()
-counter = 0
-create = 0
+
+input = {}
+
 
 
 @app.errorhandler(400)
@@ -41,16 +42,12 @@ def admin():
 
 @app.route('/notifyContext', methods=['POST'])
 def notifyContext():
-    print '=============notify============='
+    print("=============notify=============")
+
     if not request.json:
         abort(400)
 
     objs = readContextElements(request.json)
-    global counter
-    counter = counter + 1
-
-    print objs
-
     handleNotify(objs)
 
     return jsonify({'responseCode': 200})
@@ -58,20 +55,32 @@ def notifyContext():
 
 def element2Object(element):
     ctxObj = {}
+
     for key in element:
         ctxObj[key] = element[key]
+
     return ctxObj
 
 
 def object2Element(ctxObj):
+
     ctxElement = {}
+    ctxElement['id']=ctxObj['id']
+    ctxElement['type']=ctxObj['type']
+
 
     for key in ctxObj:
-        ctxElement[key] = ctxObj[key]
+        if key != "id" and key != "type" and key != "modifiedAt" and key != "createdAt" and key != "observationSpace" and key != "operationSpace" and key != "location" and key != "@context":
+            if ctxObj[key].has_key('createdAt'):
+                ctxObj[key].pop('createdAt')
+            if ctxObj[key].has_key('modifiedAt'):
+                ctxObj[key].pop('modifiedAt')
+            ctxElement[key] = ctxObj[key]
+
     return ctxElement
 
-
 def readContextElements(data):
+    print(data)
 
     ctxObjects = []
 
@@ -79,128 +88,147 @@ def readContextElements(data):
         if response['statusCode']['code'] == 200:
             ctxObj = element2Object(response['contextElement'])
             ctxObjects.append(ctxObj)
+
     return ctxObjects
 
 
 def handleNotify(contextObjs):
-    for ctxObj in contextObjs:
-        processInputStreamData(ctxObj)
-
-
-def processInputStreamData(obj):
-    print '===============receive context entity===================='
-    print obj
-
-    global counter
-    counter = counter + 1
+    fogflow.handleEntity(contextObjs, publishResult)
 
 
 def handleConfig(configurations):
     global brokerURL
-    global num_of_outputs
     for config in configurations:
         if config['command'] == 'CONNECT_BROKER':
             brokerURL = config['brokerURL']
-        if config['command'] == 'SET_OUTPUTS':
+        elif config['command'] == 'SET_OUTPUTS':
             outputs.append({'id': config['id'], 'type': config['type']})
+        elif config['command'] == 'SET_INPUTS':
+            setInput(config)
 
+def setInput(cmd):
+    global input
+    print("cmd")
+    print(cmd)
+    if 'id' in cmd:
+        input['id'] = cmd['id']
 
-def handleTimer():
-    global timer
+    input['type'] = cmd['type']
 
-    # publish the counting result
-
-    entity = {}
-    entity['id'] = 'urn:ngsi-ld:result.01'
-    entity['type'] = 'Result'
-    entity['counter'] = counter
-
-    publishResult(entity)
-    timer = threading.Timer(10, handleTimer)
-    timer.start()
-
-
-# update request on broker
-
-def update(resultCtxObj):
-    global brokerURL
-    if brokerURL.endswith('/ngsi10') == True:
-        brokerURL = brokerURL.rsplit('/', 1)[0]
-    if brokerURL == '':
-        return
-
-    ctxElement = object2Element(resultCtxObj)
-
-    id = ctxElement['id']
-    ctxElement.pop('id')
-    ctxElement.pop('type')
-    headers = {'Accept': 'application/ld+json',
-               'Content-Type': 'application/ld+json'}
-    response = requests.patch(brokerURL + '/ngsi-ld/v1/entities/' + id
-                              + '/attrs', data=json.dumps(ctxElement),
-                              headers=headers)
-    return response.status_code
-
-
-def sendDataToBroker(resultCtxObj):
-    global create
-    if create == 0:
-        response = cretaeRequest(resultCtxObj)
-        if response != 201:
-            print 'failed to send the create request'
-    else:
-        print 'Entity already has been created trying for update request....'
-        response = update(resultCtxObj)
-        if response != 204:
-            print 'failed to send the updte request'
-
-
-def publishResult(result):
-    resultCtxObj = {}
-    resultCtxObj['id'] = result['id']
-    resultCtxObj['type'] = result['type']
-    data = {}
-    data['type'] = 'Property'
-    data['value'] = result['counter']
-    resultCtxObj['count'] = data
-    sendDataToBroker(resultCtxObj)
-
-
-# create request on broker
-
-def cretaeRequest(ctxObj):
-    global brokerURL
-    global create
-    if brokerURL.endswith('/ngsi10') == True:
-        brokerURL = brokerURL.rsplit('/', 1)[0]
+def publishResult(ctxObj):
+    global brokerUR
     if brokerURL == '':
         return
 
     ctxElement = object2Element(ctxObj)
 
-    headers = {'Accept': 'application/ld+json',
-               'Content-Type': 'application/ld+json',
-               'Link': '<{{link}}>; rel="https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"; type="application/ld+json"'}
-    response = requests.post(brokerURL + '/ngsi-ld/v1/entities/',
-                             data=json.dumps(ctxElement),
+    updateCtxReq = {}
+    
+    updateCtxReq['updateAction'] = 'UPDATE'
+    updateCtxReq['contextElements'] = []
+    updateCtxReq['contextElements'].append(ctxElement)
+
+    headers = {'Accept': 'application/json',
+               'Content-Type': 'application/json'}
+    response = requests.post(brokerURL + '/updateContext',
+                             data=json.dumps(updateCtxReq), 
                              headers=headers)
-    if response.status_code == 201:
-        create = create + 1
-    if response.status_code != 201:
-        create = 1
-    return response.status_code
+    if response.status_code != 200:
+        print('failed to update context')
+        print(response.text)
+
+def fetchInputByQuery():            
+    ctxQueryReq = {}
+
+    ctxQueryReq['entities'] = []            
+    headers = {'Accept': 'application/ld+json',
+               'Content-Type': 'application/ld+json'}
+    response = requests.get(brokerURL + '/ngsi-ld/v1/entities/'+input['id'],
+                             headers=headers)
+
+    if response.status_code != 200:
+        print('failed to query the input data')
+        return {}
+    else:
+        jsonResult = response.json()  
+        
+        ctxObj = element2Object(jsonResult)
+
+        return ctxObj
+
+def requestInputBySubscription():
+    ctxSubReq = {}
+
+    ctxSubReq['entities'] = []
+    
+    if id in input:
+        ctxSubReq['entities'].append({'id': input['id'], 'isPattern': False})
+    else:                
+        ctxSubReq['entities'].append({'type': input['type'], 'isPattern': True})        
+
+    ctxSubReq['reference'] = "http://host.docker.internal:" + os.getenv('myport')
+
+    headers = {'Accept': 'application/json',
+               'Content-Type': 'application/json'}
+    response = requests.post(brokerURL + '/subscribeContext',
+                             data=json.dumps(ctxSubReq), 
+                             headers=headers)
+
+    if response.status_code != 200:
+        print('failed to query the input data')
+    else:
+        print('subscribed to the input data')
 
 
-if __name__ == '__main__':
-    handleTimer()
-
-    myport = int(os.environ['myport'])
-
-    myCfg = os.environ['adminCfg']
-    adminCfg = json.loads(myCfg)
-    handleConfig(adminCfg)
+# continuous execution to handle received notifications
+def notify2execution(): 
+    myport = int(os.getenv('myport'))
+    print("listening on port " + os.getenv('myport'))
 
     app.run(host='0.0.0.0', port=myport)
 
-    # timer.cancel()
 
+def runInOperationMode():
+    print("===== OPERATION MODEL========")
+
+    # apply the configuration received from the environmental varible
+    myCfg = os.getenv('adminCfg')
+    
+    print(myCfg)
+    
+    adminCfg = json.loads(myCfg)
+    handleConfig(adminCfg)
+
+    syncMode = os.getenv('sync') 
+    if syncMode != None and syncMode == 'yes':
+        query2execution()
+    else:
+        notify2execution()
+
+
+# one time execution triggered by query
+def query2execution():      
+    ctxObjects = fetchInputByQuery()
+    handleNotify(ctxObjects)
+
+
+def runInTestMode():
+    print("===== TEST NGSILD MODEL========")
+    #load the configuration
+    with open('config.json') as json_file:
+        config = json.load(json_file) 
+        print(config)
+        
+        handleConfig(config)                
+
+        # trigger the data processing
+        query2execution()
+
+if __name__ == '__main__':
+    parameters = sys.argv
+    
+    if len(parameters) == 2 and parameters[1] == "-o":
+        runInOperationMode()
+    else:
+        runInTestMode()
+    
